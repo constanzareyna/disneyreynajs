f10e8332953d21e7f8e12291c5bc7915942c7c34
Constanza Reyna <maxi.cocciolone@gmail.com>
2022-12-14 23:04:09 -0300

    first commit

---
+++ productos.js
@@ -0,0 +1,8 @@
+let stock_prods = [
+    {id: 1, nombre: "Magic Kingdom", tipo: "Theme park", cantidad: 1, precio: 41500, img: './img/magickingdom.jpg'},
+    {id: 2, nombre: "EPCOT", tipo: "Theme park", cantidad: 1 , precio: 39800, img: './img/epcot.jpg'},
+    {id: 3, nombre: "Animal Kingdom", tipo: "Theme park", cantidad: 1, precio: 38600, img: './img/animalkingdom.jpg'},
+    {id: 4, nombre: "Hollywood studios", tipo: "Theme park", cantidad: 1, precio: 37000, img: './img/hollywodstudios.jfif'},
+    {id: 5, nombre: "Blizzard Beach", tipo: "Aquatic park", cantidad: 1, precio: 27900, img: './img/blizard.jfif'},
+    {id: 6, nombre: "Typhoon Lagoon", tipo: "Aquatic park", cantidad: 1, precio: 26800, img: './img/typhoonlagoon.jpg'}
+];
